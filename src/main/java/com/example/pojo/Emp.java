bc42ff999d3761018cb933d6b33818e912e04fbc
All8926 <3181389202@qq.com>
2024-09-10 21:35:56 +0800

    first commit

--- src/main/java/com/example/pojo/Emp.java
+++ src/main/java/com/example/pojo/Emp.java
@@ -1,5 +1,22 @@
 package com.example.pojo;
 
+import lombok.Data;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+
+@Data
 public class Emp {
-    
+    private Integer id;
+    private String username;
+    private String name;
+    private String password;
+    private Short gender;
+    private String image;
+    private Short job;
+    private LocalDate entrydate;
+    private  Integer deptId;
+    private LocalDateTime createTime;
+    private LocalDateTime updateTime;
+
 }
