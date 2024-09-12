3de85f550daaa975efc84492846f5574300d8a37
All8926 <3181389202@qq.com>
2024-09-12 21:56:51 +0800

    Mybatis的基础操作-增删改查

--- src/main/java/com/example/mapper/EmpMapper.java
+++ src/main/java/com/example/mapper/EmpMapper.java
@@ -1,7 +1,35 @@
 package com.example.mapper;
 
-import org.apache.ibatis.annotations.Mapper;
+import com.example.pojo.Emp;
+import org.apache.ibatis.annotations.*;
+
+import java.time.LocalDate;
+import java.util.List;
 
 @Mapper
 public interface EmpMapper {
+
+    // 删除
+    @Delete("delete from emp where id = #{id}")
+    public int delete(Integer id);
+
+    // 新增
+    @Options(keyProperty = "id", useGeneratedKeys = true)   // 获取返回的主键
+    @Insert("insert into emp(username, name, gender, image, job, entrydate, dept_id, create_time, update_time)" +
+            "values (#{username},#{name},#{gender},#{image},#{job},#{entrydate},#{deptId},#{createTime},#{updateTime})")
+    public void insert(Emp emp);
+
+    //更新
+    @Update("update emp set username=#{username},name=#{name},gender=#{gender},image=#{image},job=#{job},entrydate=#{entrydate}," +
+            "dept_id=#{deptId},update_time=#{updateTime} where id = #{id}")
+    public void update(Emp emp);
+    
+    // 根据id查询
+    @Select("select * from emp where id = #{id}")
+    public Emp getById(Integer id);
+
+    //条件查询  (concat: 字符串拼接函数)
+    @Select("select * from emp where name like concat('%',#{name},'%') and gender = #{gender} and " +
+            "entrydate between #{begin} and #{end} order by update_time desc")
+    public List<Emp> getList(@Param("name") String name, @Param("gender") Short gender, @Param("begin") LocalDate begin, @Param("end") LocalDate end);
 }
--- src/test/java/com/example/DemoApplicationTests.java
+++ src/test/java/com/example/DemoApplicationTests.java
@@ -1,13 +1,69 @@
 package com.example;
 
+import com.example.mapper.EmpMapper;
+import com.example.pojo.Emp;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.util.List;
 
 @SpringBootTest
 class DemoApplicationTests {
+    @Autowired
+    private EmpMapper empMapper;
 
     @Test
-    void contextLoads() {
+    void testDelete() {
+      int num =  empMapper.delete(22);
+        System.out.println(num);
+    }
+
+    @Test
+    void testInsert(){
+        Emp emp = new Emp();
+        emp.setUsername("zhuxiaoming3");
+        emp.setName("祝小明");
+        emp.setGender((short)1);
+        emp.setImage("22.png");
+        emp.setJob((short)3);
+        emp.setDeptId(3);
+        emp.setEntrydate(LocalDate.of(2024  ,9,12));
+        emp.setCreateTime(LocalDateTime.now());
+        emp.setUpdateTime(LocalDateTime.now());
+        empMapper.insert(emp);
+
+        System.out.println(emp.getId().toString());
+    }
+
+    @Test
+    void testUpdate(){
+        Emp emp = new Emp();
+        emp.setId(23);
+        emp.setUsername("zhuxiaoming4");
+        emp.setName("祝小明");
+        emp.setGender((short)0);
+        emp.setImage("44.png");
+        emp.setJob((short)3);
+        emp.setDeptId(3);
+        emp.setEntrydate(LocalDate.of(2024  ,9,12));
+        emp.setCreateTime(LocalDateTime.now());
+        emp.setUpdateTime(LocalDateTime.now());
+        empMapper.update(emp);
+    }
+
+    @Test
+    void testGetById(){
+        Emp emp = empMapper.getById(5);
+        System.out.println(emp);
+    }
+
+    @Test
+   void testGetList(){
+        List<Emp> empList = empMapper.getList("小", (short) 1, LocalDate.of(2000, 1, 12), LocalDate.of(2025, 1, 1));
+        System.out.println(empList);
     }
 
 }
