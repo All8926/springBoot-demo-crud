988f690bf75203d316b555fd7d302ce80160eaee
All8926 <3181389202@qq.com>
2024-09-17 19:17:25 +0800

    员工管理-增删改查、文件上传

--- src/main/java/com/example/service/impl/EmpServiceImpl.java
+++ src/main/java/com/example/service/impl/EmpServiceImpl.java
@@ -7,8 +7,10 @@ import com.example.service.EmpService;
 import com.github.pagehelper.Page;
 import com.github.pagehelper.PageHelper;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.stereotype.Service;
 
+import java.time.LocalDate;
 import java.util.List;
 
 @Service
@@ -18,17 +20,29 @@ public class EmpServiceImpl implements EmpService {
     private EmpMapper empMapper;
 
     @Override
-    public PageBean page(Integer page, Integer pageSize) {
+    public PageBean page(Integer page, Integer pageSize, String name, Short gender,
+                         @DateTimeFormat(pattern = "yyyy-MM-dd") LocalDate begin,
+                         @DateTimeFormat(pattern = "yyyy-MM-dd") LocalDate end) {
         // 1.设置分页参数
-        PageHelper.startPage(page,pageSize);
+        PageHelper.startPage(page, pageSize);
 
         // 2.执行查询
-        List<Emp> list = empMapper.list();
+        List<Emp> list = empMapper.getList(name, gender, begin, end);
         Page<Emp> p = (Page<Emp>) list;
 
         // 3.封装PageBaen对象
         PageBean pageBean = new PageBean(p.getTotal(), p.getResult());
 
         return pageBean;
+    }
+
+    @Override
+    public void delete(List<Integer> ids) {
+        empMapper.deleteByIds(ids);
+    }
+
+    @Override
+    public void save(Emp emp) {
+        empMapper.insert(emp);
     }
 }
